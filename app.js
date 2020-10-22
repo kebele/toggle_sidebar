36966d173669a6386612a2b5d0be0ee7fd3cc6b1
kebele <51454858+kebele@users.noreply.github.com>
2020-10-22 10:32:24 +0300

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("side-menu").style.width = "300px";
+    document.getElementById("content-area").style.marginLeft = "300px"
+}
+function closeNav(){
+    document.getElementById("side-menu").style.width = "0px";
+    document.getElementById("content-area").style.marginLeft = "0px"
+}
+
